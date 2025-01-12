2a539903be2df259342849916e0a2173d76a8fc4
Annuar Mulombi <mulombiannuar@gmail.com>
2025-01-12 22:23:55 +0300

    continuous improvement

--- resources/views/app/pages/partials/header_content.blade.php
+++ resources/views/app/pages/partials/header_content.blade.php
@@ -1,5 +1,5 @@
 <div class="content-block">
-    <h1>Track Bills, Simplified</h1>
+    <h1>Track Bills, Simplified!</h1>
     <p>Empowering Kenyans to stay informed and engaged. Track parliamentary bills with ease and
         transparency – follow the progress of proposed laws, understand key issues, and participate in
         the democratic process. Stay up-to-date and make your voice heard in shaping the future of Kenya
