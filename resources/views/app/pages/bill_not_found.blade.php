c501e6025e423ca92c30aff58ad65dbbd3e6bbfe
Annuar Mulombi <mulombiannuar@gmail.com>
2025-03-18 13:34:55 +0300

    added more function

--- resources/views/app/pages/bill_not_found.blade.php
+++ resources/views/app/pages/bill_not_found.blade.php
@@ -7,7 +7,7 @@
                     No bills found for the specified user since the user does not exist.
 
                 </div>
-                <a href="{{ route('search') }}" class="btn btn-contact d-inline-block btn-primary px-lg-5 my-1 px-md-3">
+                <a href="{{ route('search') }}" class="btn btn-contact d-inline-block btn-success px-lg-5 my-1 px-md-3">
                     Click here to View all bills</a>
             </div>
         </div>
